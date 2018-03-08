1dad8a358d7a69433844d5beabc142b28e438f13
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-03-08 15:29:42 -0700

    First commit, Html pretty much done, needs js and css

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA7JsGew_Vy3hGnM2FIXJlrcpHhw0rNIF0",
+    authDomain: "trainschedulefirebase.firebaseapp.com",
+    databaseURL: "https://trainschedulefirebase.firebaseio.com",
+    projectId: "trainschedulefirebase",
+    storageBucket: "trainschedulefirebase.appspot.com",
+    messagingSenderId: "649916968242"
+  };
+  firebase.initializeApp(config);
