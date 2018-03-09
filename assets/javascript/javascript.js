32ff3ec83e532989b85961060370cd8099b044f5
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-03-09 00:04:15 -0700

    grabbing user input, tracking time, created objects to hold inputs, all console logging

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,3 +8,42 @@
     messagingSenderId: "649916968242"
   };
   firebase.initializeApp(config);
+
+  //Variable to reference database
+  var database = firebase.database();
+
+  //Declare current time
+  var currentTime = moment().format();
+    //Logging the current time-Working!
+    console.log("Current Time: " + currentTime);
+   
+$("click-button").on("click", function() {
+    event.preventDefault(); 
+
+    //Grabbing user input
+    var trainNameForm = ("#trainNameForm").val().trim(); 
+    var destinationForm = ("#destinationForm").val().trim();
+    //Tracking time 
+    var trainTimeForm = moment($("#trainTimeForm").val().trim(), "HH:mm").format("HH:mm");
+    var frequencyForm = ("#frequencyForm").val().trim();
+    //creating local "temporary" objects for holding inputs
+    var newTrain = {
+        train: trainNameForm,
+        destination: destinationForm,
+        first: trainTimeForm,
+        frequency: frequencyForm,
+
+    };
+
+    //setting new values in the database
+    database.ref().push(newTrain);
+
+    //console logging to make sure the new data has been stored to database
+    console.log(newTrain.train);
+    console.log(newTrain.destination);
+    console.log(newTrain.first);
+    console.log(newTrain.frequency);
+
+    
+
+});
